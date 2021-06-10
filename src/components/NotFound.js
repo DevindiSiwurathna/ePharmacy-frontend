bbe323cf57990b7e978820af64ec0809ca092946
Devindi <devindisiwurathna@gmail.com>
2021-06-10 14:12:51 +0530

    Notfound

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,7 +3,9 @@ import React from "react";
 const NotFound = () => {
   return (
     <div className="not-found">
-      <h1 className="display-1">Error !Page Not Found</h1>
+      <h1 className="display-1">Error 404 - Page Not Found</h1>
+      
+      
     </div>
   );
 };
